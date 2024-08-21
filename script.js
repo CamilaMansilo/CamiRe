ae7674a3e7a2d647922103ce4851f39ef627153f
CamilaMansilo <camilamansilo@gmail.com>
2024-08-20 21:53:06 -0300

    Ejer de clase

---
+++ script.js
@@ -0,0 +1,12 @@
+window.onload=function(){
+
+var boton = document.getElementById("boton");
+
+boton.addEventListener("mouseover", function() {
+    boton.style.display="none";
+
+    setTimeout(() => {
+        boton.style.display="block";
+    }, 4000);
+ });
+}
